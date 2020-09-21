ac172b5898d275615b45b79732352fe2c1de4e2a
Frank Mcmanus <frankmcmanus@Franks-MBP-2.home>
2020-09-21 09:42:34 -0400

    implement themeprovider for gradient bg

--- src/context/ThemeStore.js
+++ src/context/ThemeStore.js
@@ -1,45 +1,47 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  Animated,    
+  Button,
+  Text,
+  TouchableOpacity } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 const ThemeContext = React.createContext();
 
-const ThemeProvider = ({children}) => {
+const ThemeProvider = ({ children }) => {
+
+  const gradientColors = {
+    bottomDeepBlue: '#2d24bd',
+    bottomMidBlue: '#3963c7',
+    topLightPeach: '#ff8960',
+    bottomTeal: '#22c1c3',
+    topGold: '#fdbb2d',
+    bottomBlue: '#3f5efb',
+    topMagenta: '#fc466b',
+    bottomPastelBlue: '#94bbe9',
+    topPastelPink: '#eeaeca',
+  };
+
   const themes = {
-    themeOne: {
-      gradientTop: '#CCD1BA',
-      gradientBottom: '#BBCFD0',
-    },
-    themeTwo: {
-      gradientTop: '#E1D4F0',
-      gradientBottom: '#BEC9D4',
-    },
+    Home:
+      [ gradientColors.bottomDeepBlue,
+        gradientColors.bottomMidBlue,
+        gradientColors.topLightPeach ],
+    themeTwo: [ gradientColors.bottomMidBlue,
+      gradientColors.bottomPastelBlue,
+      gradientColors.topPastelPink ],
     themeThree: {
       gradientTop: '#E79E84',
       gradientBottom: '#E5CB99',
     },
   };
 
-  const meditationList = [
-    {
-      name: 'Meditation One',
-      id: 1,
-    },
-    {
-      name: 'Meditation Two',
-      id: 2,
-    },
-    {
-      name: 'Meditation Three',
-      id: 3,
-    },
-    {
-      name: 'Meditation Four',
-      id: 4,
-    },
-  ];
-
-  const [theme, setTheme] = useState(themes['themeOne']);
+  const [theme, setTheme] = useState(themes['Home']);
 
   const switchTheme = (theme) => {
+    console.log('them from switchtheme ', theme)
     if (theme in themes) {
       setTheme(themes[theme]);
     } else {
@@ -48,10 +50,10 @@ const ThemeProvider = ({children}) => {
   };
 
   return (
-    <ThemeContext.Provider value={{theme, meditationList, switchTheme}}>
+    <ThemeContext.Provider value={{ theme, switchTheme }}>
       {children}
     </ThemeContext.Provider>
   );
 };
 
-export {ThemeProvider, ThemeContext};
+export { ThemeProvider, ThemeContext };
