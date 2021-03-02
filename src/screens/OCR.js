9f5fac98e482db447034d6df932cb0382700c41d
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2021-03-02 13:22:11 -0500

    Add OCR Screen

---
+++ src/screens/OCR.js
@@ -0,0 +1,30 @@
+import React, {useContext} from 'react';
+import styled from 'styled-components/native';
+import AnimationRenderer from '../components/AnimationRenderer';
+import animationData from '../assets/cloud.json';
+import {Text, Button} from 'react-native';
+
+const Container = styled.View`
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  background: white;
+`;
+
+const OCR = (props) => {
+  const {navigation, route} = props;
+
+  return (
+    <Container>
+      <Text>{route.params ? route.params.name : 'OCR'}</Text>
+      <AnimationRenderer
+        animationData={animationData}
+        autoPlay={true}
+        resizeMode="cover"
+      />
+      <Button title="Back" onPress={() => navigation.navigate('Home')} />
+    </Container>
+  );
+};
+
+export default OCR;
