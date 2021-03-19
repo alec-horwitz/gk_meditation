9818cffac9854824f60f4fe08a54919ae36fc2b1
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2021-03-18 21:20:37 -0400

    implement successful image upload

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const express = require('express')
-// const multer = require('multer')
+const cors = require("cors");
+const multer = require('multer')
 const bodyParser = require('body-parser')
 const tesseract = require("node-tesseract-ocr")
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 
-// const Storage = multer.diskStorage({
-//   destination(req, file, callback) {
-//     callback(null, './images');
-//   },
-//   filename(req, file, callback) {
-//     callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
-//   },
-// });
+const Storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, './uploaded_images');
+  },
+  filename(req, file, callback) {
+    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
+  },
+});
 
-// const upload = multer({ storage: Storage });
+const upload = multer({ storage: Storage });
 
 runTesseract = async () => {
 	const config = {
@@ -49,13 +52,24 @@ runTesseract = async () => {
 	return result
 }
 
-app.post('/api/upload', (req, res) => {
-    // console.log('File: ', req.files);
-	console.log('Body: ', req.body);
-    // res.send(`${await runTesseract()}`)
-    res.status(200).json({
-	    message: 'success!',
-	  });
+app.post('/api/upload', upload.any(), (req, res, next) => {
+	console.log('typeof req: ', typeof req);
+	console.log('req: ',Object.keys(req));
+    console.log('files: ', req.files);
+    console.log("file =>", req.file);
+	console.log('body: ', req.body);
+	console.log('params: ', req.params);
+    // req.send(`${await runTesseract()}`)
+    if (req.files || req.file) {
+	    res.status(200).json({
+			message: 'Success!',
+		});
+    } else {
+		console.log('Insufficient data! Please upload a file!');
+		const error = new Error('Insufficient data! Please upload a file!')
+	    error.httpStatusCode = 400
+	    return next(error)
+    }
 });
 
 app.get('/', async (req, res) => {
@@ -63,5 +77,5 @@ app.get('/', async (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Tesseract node server listening at http://localhost:${port}`)
 })
--- src/screens/OCR.js
+++ src/screens/OCR.js
@@ -1,106 +1,213 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import styled from 'styled-components/native';
-import AnimationRenderer from '../components/AnimationRenderer';
-import animationData from '../assets/cloud.json';
-import {Text, Button, StyleSheet, View, TouchableOpacity} from 'react-native';
-import {RNCamera} from 'react-native-camera';
+// import AnimationRenderer from '../components/AnimationRenderer';
+// import animationData from '../assets/cloud.json';
+import {Text, Button, StyleSheet, View, Image, TouchableOpacity} from 'react-native';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 // import RNFS from 'react-native-fs';
 // import { v4 as uuidv4 } from 'uuid';
 
-const Container = styled.View`
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-  background: white;
-`;
 
-let cameraObj: any;
 
-const OCR = (props) => {
-  const {navigation, route} = props;
 
-  let capturedPhoto = null;
-  let OCResponse = null
 
-  runOCR = async (photoPath) => {
-
-    // const photo = {
-    //   uri: Platform.OS === 'android' ? photoPath : photoPath.replace('file://', ''),
-    //   type: "image/jpg",
-    //   name: photoPath.slice(photoPath.lastIndexOf("/")+1),
-    // };
-
-    // const form = new FormData();
-
-    // form.append("photo", photo);
-    // form.append("userId", uuidv4())
-
-    // RNFS.readFile(photoPath, 'base64')
-    // .then(base64photo =>{
-    //   console.log(base64photo);
-      fetch('http://192.168.0.184:3000/api/upload', {
-        method: 'post',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({text: "Hello World! Post Response!"})
-      }).catch(function(error) {
-        console.log('There has been a problem with your fetch operation: ' + error.message);
-      });
-      // .then(res => res.json()).then(res => {
-      //   console.log(res)
-      //   OCResponse = res
-      // })
-    // });
+class OCR extends React.Component {
+  state = {
+    photo: null,
   }
 
-  takePicture = async () => {
-    if (cameraObj) {
-      const options = { quality: 0.5, base64: true };
-      const data = await cameraObj.takePictureAsync(options);
-      capturedPhoto = data.uri
-    }
-    console.log(capturedPhoto)
-    runOCR(capturedPhoto)
+  // createFormData = (photo) => {
+  //   const data = new FormData();
+
+  //   data.append("photo", {
+  //     name: photo.fileName,
+  //     type: photo.type,
+  //     uri:
+  //       Platform.OS === "android" ? photo.uri : photo.uri.replace("file://", "")
+  //   });
+  //   console.log("Hello World! data: ", data)
+
+  //   return data;
+  // };
+
+  handleUploadPhoto = () => {
+    const {photo} = this.state
+
+    const data = new FormData();
+
+    data.append("photo", {
+      name: photo.fileName,
+      type: photo.type,
+      uri: Platform.OS === "android" ? photo.uri : photo.uri.replace('file://', '/private')
+    })
+
+    console.log("Hello World! data: ", data)
+
+    fetch('http://192.168.0.184:3000/api/upload', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: data
+    })
+      .then(response => response.json())
+      .then(response => {
+        console.log("upload succes", response);
+        alert("Upload success!");
+        this.setState({ photo: null });
+      })
+      .catch(error => {
+        console.log("upload error", error);
+        alert("Upload failed!");
+      });
   };
 
-  return (
-    <View style={{flex: 1}}>
-      <RNCamera
-        ref={e => (cameraObj = e)}
-        style={styles.preview}
-        type={RNCamera.Constants.Type.back}
-        flashMode={RNCamera.Constants.FlashMode.off}
-        androidCameraPermissionOptions={{
-          title: 'Permission to use camera',
-          message: 'We need your permission to use your camera',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}
-        androidRecordAudioPermissionOptions={{
-          title: 'Permission to use audio recording',
-          message: 'We need your permission to use your audio',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}
-        onGoogleVisionBarcodesDetected={({barcodes}) => {
-          console.log(barcodes);
-      }}>
-      </RNCamera>
-      <View style={styles.frameContainer}>
-        <Text style={styles.titleText}>{route.params ? route.params.name : 'OCR'}</Text>
-        <View>{OCResponse}</View>
-        <View>
-          <TouchableOpacity onPress={()=>takePicture()} style={styles.capture}>
-            <Text> SNAP </Text>
-          </TouchableOpacity>
-          <Button title="Back" onPress={() => navigation.navigate('Home')} />
+  handleChoosePhoto = () => {
+    const options = {
+      noData: true,
+    }
+    launchCamera(
+      {
+        mediaType: 'photo',
+        cameraType: 'back',
+        includeBase64: false,
+      },
+      (response) => {
+        console.log("Hello World! response: ", response);
+        this.setState({photo: response});
+      },
+    )
+    // ImagePicker.launchImageLibrary(options, response => {
+    //   if (response) {
+    //     this.setState({ photo: response })
+    //   }
+    // })
+  }
+
+  render() {
+    const { photo } = this.state
+    const {navigation, route} = this.props;
+    photo && this.handleUploadPhoto()
+    return (
+      <View style={{ flex: 1 }}>
+        {photo && (
+          <Image
+            source={{ uri: photo.uri }}
+            style={{ width: 300, height: 300 }}
+          />
+        )}
+        <View style={styles.frameContainer}>
+          <Text style={styles.titleText}>{route.params ? route.params.name : 'OCR'}</Text>
+          <View>
+            <TouchableOpacity onPress={this.handleChoosePhoto} style={styles.capture}>
+              <Text> SNAP </Text>
+            </TouchableOpacity>
+            <Button title="Back" onPress={() => navigation.navigate('Home')} />
+          </View>
         </View>
       </View>
-    </View>
-  );
-};
+    )
+  }
+}
+
+
+
+
+
+// const Container = styled.View`
+//   align-items: center;
+//   justify-content: center;
+//   flex: 1;
+//   background: white;
+// `;
+
+// let cameraObj: any;
+
+// const OCR = (props) => {
+//   const {navigation, route} = props;
+
+//   let capturedPhoto = null;
+//   let OCResponse = null
+
+//   runOCR = async (photoPath) => {
+
+//     // const photo = {
+//     //   uri: Platform.OS === 'android' ? photoPath : photoPath.replace('file://', ''),
+//     //   type: "image/jpg",
+//     //   name: photoPath.slice(photoPath.lastIndexOf("/")+1),
+//     // };
+
+//     // const form = new FormData();
+
+//     // form.append("photo", photo);
+//     // form.append("userId", uuidv4())
+
+//     // RNFS.readFile(photoPath, 'base64')
+//     // .then(base64photo =>{
+//     //   console.log(base64photo);
+//       fetch('http://192.168.0.184:3000/api/upload', {
+//         method: 'post',
+//         headers: {
+//           Accept: 'application/json',
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({text: "Hello World! Post Response!"})
+//       }).catch(function(error) {
+//         console.log('There has been a problem with your fetch operation: ' + error.message);
+//       });
+//       // .then(res => res.json()).then(res => {
+//       //   console.log(res)
+//       //   OCResponse = res
+//       // })
+//     // });
+//   }
+
+//   takePicture = async () => {
+//     if (cameraObj) {
+//       const options = { quality: 0.5, base64: true };
+//       const data = await cameraObj.takePictureAsync(options);
+//       capturedPhoto = data.uri
+//     }
+//     console.log(capturedPhoto)
+//     // runOCR(capturedPhoto)
+//   };
+
+//   return (
+//     <View style={{flex: 1}}>
+//       <RNCamera
+//         ref={e => (cameraObj = e)}
+//         style={styles.preview}
+//         type={RNCamera.Constants.Type.back}
+//         flashMode={RNCamera.Constants.FlashMode.off}
+//         androidCameraPermissionOptions={{
+//           title: 'Permission to use camera',
+//           message: 'We need your permission to use your camera',
+//           buttonPositive: 'Ok',
+//           buttonNegative: 'Cancel',
+//         }}
+//         androidRecordAudioPermissionOptions={{
+//           title: 'Permission to use audio recording',
+//           message: 'We need your permission to use your audio',
+//           buttonPositive: 'Ok',
+//           buttonNegative: 'Cancel',
+//         }}
+//         onGoogleVisionBarcodesDetected={({barcodes}) => {
+//           console.log(barcodes);
+//       }}>
+//       </RNCamera>
+//       <View style={styles.frameContainer}>
+//         <Text style={styles.titleText}>{route.params ? route.params.name : 'OCR'}</Text>
+//         <View>{OCResponse}</View>
+//         <View>
+//           <TouchableOpacity onPress={()=>takePicture()} style={styles.capture}>
+//             <Text> SNAP </Text>
+//           </TouchableOpacity>
+//           <Button title="Back" onPress={() => navigation.navigate('Home')} />
+//         </View>
+//       </View>
+//     </View>
+//   );
+// };
 
 const styles = StyleSheet.create({
   frameContainer: {
